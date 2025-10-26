8004090686c6587aa94f3a104c6481ae63cbe39a
Saydva <kacurikjozef@gmail.com>
2025-10-26 13:08:34 +0100

    repared sptps view mobile devs

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,4 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/ProductPage/', // Zmeňte na názov vášho GitHub repozitára
-  css: {
-    preprocessorOptions: {
-      scss: {
-        silenceDeprecations: ['legacy-js-api', 'import'],
-        quietDeps: true,
-        charset: false,
-      },
-    },
-  },
 });
